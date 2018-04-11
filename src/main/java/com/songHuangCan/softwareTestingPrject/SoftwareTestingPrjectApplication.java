d692c29921ed868b3eaee974afc5eb34ca0cc92d
yansong <yansongsongsong@gmail.com>
2018-04-11 18:19:18 +0800

    init spring boot pro

---
+++ src/main/java/com/songHuangCan/softwareTestingPrject/SoftwareTestingPrjectApplication.java
@@ -0,0 +1,12 @@
+package com.songHuangCan.softwareTestingPrject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareTestingPrjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareTestingPrjectApplication.class, args);
+	}
+}
